7ef57a988fed562c1a60b2ad0a41407980867d26
Swapnil Ingle <ingleswapnil2004@gmail.com>
2025-08-30 14:16:31 +0530

    basics

--- CPP/basics/cppBasics/hello.cpp
+++ CPP/basics/cppBasics/hello.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    cout << "Hello World" << endl;
+int add(int a, int b)
+{
+    return a + b;
+}
+int main()
+{
+    cout << "Hello World from C++" << add(23, 2323) << endl;
     return 0;
 }
